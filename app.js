0b91adf8454709d7246b439e5ae2b4a8103c2733
DARSHAN SAKPAL <d.sakpal38@gmail.com>
2019-07-22 20:07:33 +0530

    Fixed the bugs by hididng the roll and hold button but we can have a better way to implement it.

--- app.js
+++ app.js
@@ -6,26 +6,8 @@ scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-//generate random number for dice
 
-
-//display the dice value to the currentscore
-// document.querySelector('#current-' + activePlayer).textContent = dice;
-
-//store the text content to the variable
-// var x = document.querySelector("#score-0").textContent;
-// console.log(x);
-
-//hide the dice image
-document.querySelector('.dice').style.display = 'none';
-
-//initialize all values to zero
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
-
-
+init();
 //use event listener for dice roll
 document.querySelector('.btn-roll').addEventListener('click', function(){
 	//Random number.
@@ -60,7 +42,8 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 	if (scores[activePlayer] >= 10){
 		document.querySelector('#name-'+ activePlayer).textContent = 'WINNER';
 		document.querySelector('.dice').style.display = 'none';
-		document.querySelector('.player-'+activePlayer+'-panel').classList.toggle('active');
+		document.querySelector('.player-'+activePlayer+'-panel').classList.add('winner');
+		document.querySelector('.player-'+activePlayer+'-panel').classList.remove('active');
 		document.querySelector('.btn-roll').style.display = 'none';
 		document.querySelector('.btn-hold').style.display = 'none';
 	}
@@ -71,20 +54,15 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 });
 
 //use event listener on new game button
-document.querySelector('.btn-new').addEventListener('click', function(){
-	//nullify all the score
-	document.querySelector('#current-0').textContent ='0';
-	document.querySelector('#current-1').textContent ='0';
-	document.querySelector('#score-0').textContent ='0';
-	document.querySelector('#score-1').textContent ='0';
+document.querySelector('.btn-new').addEventListener('click', init
 
 	//display the roll btn and the hold btn
-	document.querySelector('.btn-hold').style.display = 'block';
-	document.querySelector('.btn-roll').style.display = 'block';
+	// document.querySelector('.btn-hold').style.display = 'block';
+	// document.querySelector('.btn-roll').style.display = 'block';
 
 	//make player zero active again
-	document.querySelector('player-0-panel').classList.add('active');
-});
+	// document.querySelector('player-0-panel').classList.add('active');
+);
 
 
 function nextPlayer(){
@@ -99,4 +77,31 @@ function nextPlayer(){
 
 		//hide the dice while toggle
 		document.querySelector('.dice').style.display = 'none';
+}
+
+
+function init(){
+	scores = [0,0];
+	roundScore = 0;
+	activePlayer = 0;
+
+	//hide the dice image
+	document.querySelector('.dice').style.display = 'none';
+
+	//initialize all values to zero
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById('name-0').textContent = 'PLAYER 1';
+	document.getElementById('name-1').textContent = 'PLAYER 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	// document.querySelector('.player-1-panel').classList.add('active');
+	document.querySelector('.btn-roll').style.display = 'block';
+	document.querySelector('.btn-hold').style.display = 'block';
+
 }
